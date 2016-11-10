eb0aa408130650c11388df2f004645c9fd1cf86d
mattia richetto <mrichetto@opentable.com>
2016-11-10 23:29:59 +0000

    add webtask_no_cache=1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,7 +4,7 @@
   // ```bash
   // wt edit
   // ```
-  const request = pegasus('https://wt-mattia-richetto-gmail-com-0.run.webtask.io/arabic2roman?arabic=2016');
+  const request = pegasus('https://wt-mattia-richetto-gmail-com-0.run.webtask.io/arabic2roman?webtask_no_cache=1&arabic=2016');
   request.then(
     (data, xhr) => {
       document.getElementById('main-title').innerHTML = data.roman;
