df19def55eb56d6bd6528a442bc2cc40c4e6eefa
mattia richetto <mrichetto@opentable.com>
2016-11-10 23:10:42 +0000

    use Webtask

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,10 @@
 ((pegasus) => {
   // https://sad-balcony.herokuapp.com/converter/arabic2roman/2016
-  const request = pegasus('https://sad-balcony.herokuapp.com/converter/arabic2roman/2016');
+
+  // ```bash
+  // wt edit
+  // ```
+  const request = pegasus('https://wt-mattia-richetto-gmail-com-0.run.webtask.io/arabic2roman?arabic=2016');
   request.then(
     (data, xhr) => {
       document.getElementById('main-title').innerHTML = data.roman;
