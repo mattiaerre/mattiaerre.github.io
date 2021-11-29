d28508737011de24fd9ff32f7174ca5efed551ab
mattia richetto <mattia.richetto@gmail.com>
2021-11-29 10:07:37 -0500

    remove all the things

--- src/client-v2/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // eslint-disable-line
-import { render } from 'react-dom';
-import App from './components/App/App'; // eslint-disable-line
-
-render(
-  <App />,
-  document.getElementById('app')
-);
